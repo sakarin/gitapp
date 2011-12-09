843c5fa0717165a811ee172b763f81fe14107524
Sakarin Radchapunya <sakarin.my@gmail.com>
2011-12-09 14:29:05 +0700

    start product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitapp::Application.config.secret_token = 'b2a23e3eaf65109fa49f3052df8f9a75d369b53db6ec96cd23ff77e9d76bfb5d5950d78ad341c09ec6ba9bf9b5bfac102a1f8217ef0cbcf341d45f813d9eb998'
